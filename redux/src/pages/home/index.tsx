3e4c4475c54d5d722de3c6685055584778885a76
김정환 <jh.kim@woowahan.com>
2019-07-14 10:29:28 +0900

    메모장 홈 페이지 구현

--- redux/src/pages/home/index.tsx
+++ redux/src/pages/home/index.tsx
@@ -5,18 +5,26 @@ import Sidebar, { SidebarTitle } from '../../components/Sidebar';
 import Main from '../../components/Main';
 import AddMemoBtn from '../../components/AddMenuBtn';
 import { List, ListItem } from '../../components/List';
+import { Memo } from '../../models';
 
-const HomePage: React.FC = () => {
+interface Props {
+  memos: Memo[]
+  deletedMemos: Memo[] 
+}
+
+const HomePage: React.FC<Props> = props => {
+  const { memos, deletedMemos } = props
+  
   return (
     <Layout>
       <Sidebar>
         <SidebarTitle>폴더</SidebarTitle>
         <List>
           <ListItem first>
-            <Link to="/memo">메모</Link>
+            <Link to="/memo">메모({memos.length})</Link>
           </ListItem>
           <ListItem>
-            <Link to="/trash">휴지통</Link>
+            <Link to="/trash">휴지통({deletedMemos.length})</Link>
           </ListItem>
         </List>
       </Sidebar>
